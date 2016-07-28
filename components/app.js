774016dff53c0327bd44a68bda5ba3692556b7d8
pplinlin2 <pplinlin2@gmail.com>
2016-07-28 17:50:22 +0800

    bindActionCreators used

--- components/app.js
+++ components/app.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 
 import { increase, decrease } from '../actions';
 
-@connect((store) => ({ counter: store.counter }))
+@connect(
+    (store) => ({ counter: store.counter }), 
+    (dispatch) => (bindActionCreators({increase, decrease}, dispatch)), 
+)
 export default class App extends Component {
     render() {
-        const { counter, dispatch } = this.props;
+        const { counter } = this.props;
 
         // counter from the connect(select) and dispatch by default from connect
         return (
             <div>
                 <h1>{counter}</h1>
                 <ButtonToolbar>
-                    <Button bsStyle="primary" onClick={ () => dispatch(increase())}>Increase</Button>
-                    <Button bsStyle="danger" onClick={ () => dispatch(decrease())}>Decrease</Button>
+                    <Button bsStyle="primary" onClick={ () => this.props.increase() }>Increase</Button>
+                    <Button bsStyle="danger" onClick={ () => this.props.decrease() }>Decrease</Button>
                 </ButtonToolbar>
             </div>
         );
--- components/async.js
+++ components/async.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
 import { fetchuser } from '../actions';
 
-@connect((store) => ({users: store.users}))
+@connect(
+    (store) => ({users: store.users}), 
+    (dispatch) => (bindActionCreators({fetchuser}, dispatch)), 
+)
 export default class App extends Component {
     render() {
-        const { users, dispatch } = this.props;
+        const { users } = this.props;
 
         if (!users.fetched) {
-            return <Button onClick={() => dispatch(fetchuser())}>Load users</Button>
+            return <Button onClick={ () => this.props.fetchuser() }>Load users</Button>
         }
 
         const mappedUsers = users.users.map(user => <li key={user.id}>{user.name}</li>)
         return (
             <div>
-                <Button onClick={() => dispatch(fetchuser())}>Load users</Button>
+                <Button onClick={ () => this.props.fetchuser() }>Load users</Button>
                 <ul>{mappedUsers}</ul>
             </div>
         )
